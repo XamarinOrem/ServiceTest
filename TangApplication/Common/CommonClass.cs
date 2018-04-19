fff304896bb851a639f2f6032a28448fe68d4c85
DESKTOP-J1579HK\win 10 <harinder.orem@gmail.com>
2018-04-18 17:30:29 -0700

    Add project files.

---
+++ TangApplication/Common/CommonClass.cs
@@ -0,0 +1,96 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TangApplication.Models;
+
+namespace TangApplication.Common
+{
+    
+    public class CommonClass
+    {
+        public static string AppUrl = "http://api.faitango.it";
+        /// <summary>
+        /// Created By: Harinder Kaur
+        /// Created Date: 28 Dec, 2016
+        /// Descrition: Get News List 
+        /// <param>Url(url of webservice which return json data)</param>
+        /// <returns>class(News List data)</returns>
+        ///</summary>
+        public static async Task<wsNewsModel> GetNewsList(string url)
+        {
+            wsNewsModel objData = new wsNewsModel();
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(url);
+                    var result = await client.GetAsync(url);
+                    objData = JsonConvert.DeserializeObject<wsNewsModel>(await result.Content.ReadAsStringAsync());
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+            }
+            return objData;
+        }
+
+        /// <summary>
+        /// Created By: Harinder Kaur
+        /// Created Date: 28 Dec, 2016
+        /// Descrition: Get Service Data List 
+        /// <param>Url(url of webservice which return json data)</param>
+        /// <returns>class(Service data)</returns>
+        ///</summary>
+        public static async Task<wsTypeModel> GetServiceData(string url)
+        {
+            wsTypeModel objData = new wsTypeModel();
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(url);
+                    var result = await client.GetAsync(url);
+                    objData = JsonConvert.DeserializeObject<wsTypeModel>(await result.Content.ReadAsStringAsync());
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+            }
+            return objData;
+        }
+        /// <summary>
+        /// Created By: Harinder Kaur
+        /// Created Date: 29 Dec, 2016
+        /// Descrition: Get Service Data List 
+        /// <param>Url(url of webservice which return json data)</param>
+        /// <returns>class(Service List data)</returns>
+        ///</summary>
+        public static async Task<wsFilterDropDownsModel> GetFiltersDropDowns(string url)
+        {
+            wsFilterDropDownsModel objData = new wsFilterDropDownsModel();
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(url);
+                    var result = await client.GetAsync(url);
+                    objData = JsonConvert.DeserializeObject<wsFilterDropDownsModel>(await result.Content.ReadAsStringAsync());
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+            }
+            return objData;
+        }
+    }
+}
---
+++ TangApplication/Models/NewsModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TangApplication.Models
+{
+    public class NewsModel
+    {
+        public string post_date { get; set; }
+        public string Post_Date
+        {
+            get
+            {
+                return Convert.ToDateTime(post_date).ToString("dd-MM-yyyy"); 
+            }
+           
+        }
+        public string post_content { get; set; }
+        public string post_title { get; set; }
+        public string post_name { get; set; }
+        public string post_img { get; set; }
+        public string post_url { get; set; }
+
+        public string Post_Url
+        {
+            get
+            {
+                return post_url.Replace(@"\/", "/"); 
+            }
+          
+        }
+    }
+
+    public class wsNewsModel
+    {
+        public string mesg { get; set; }
+        public string responce { get; set; }
+        public List<NewsModel> data { get; set; }
+    }
+}
